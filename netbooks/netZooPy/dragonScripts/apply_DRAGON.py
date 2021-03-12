02982fc3fef7d3701d1038483eac1555a94a9a1f
marouenbg <marouen.b.guebila@gmail.com>
2021-03-12 00:00:31 -0500

    fix url dragon

--- netbooks/netZooPy/dragonScripts/apply_DRAGON.py
+++ netbooks/netZooPy/dragonScripts/apply_DRAGON.py
@@ -6,8 +6,8 @@ from netZooPy.dragon import *
 #import copy
 #import numpy as np
 
-XA = np.load("../../data/XA.npy")#[:,0:20]
-XB = np.load("../../data/XB.npy")#[:,0:10]
+XA = np.load("../data/XA.npy")#[:,0:20]
+XB = np.load("../data/XB.npy")#[:,0:10]
 pA = XA.shape[1]
 pB = XB.shape[1]
 n = XA.shape[0]
@@ -24,8 +24,8 @@ lams, _ = estimate_penalty_parameters_dragon(XA, XB)
 print('lambdas = ',lams)
 
 r = get_partial_correlation_dragon(XA, XB, lams)
-np.save("../../data/par_cor.npy", r)
+np.save("../data/par_cor.npy", r)
 
 adj_p_vals, p_vals = estimate_p_values_dragon(r, n, pA, pB, lams)
-np.save("../../data/adj_p_vals.npy", adj_p_vals)
-np.save("../../data/p_vals.npy", p_vals)
+np.save("../data/adj_p_vals.npy", adj_p_vals)
+np.save("../data/p_vals.npy", p_vals)
--- netbooks/netZooPy/dragonScripts/apply_GGM.py
+++ netbooks/netZooPy/dragonScripts/apply_GGM.py
@@ -1,7 +1,7 @@
 import numpy as np
 from netZooPy.dragon import *
 
-X = np.load("../../data/X.npy")
+X = np.load("../data/X.npy")
 X_std = np.std(X, axis=0)
 X_mean = np.mean(X, axis=0)
 X = (X - X_mean) / X_std
@@ -14,6 +14,6 @@ print('lambda = ',lam)
 r = get_partial_correlation(X, lam)
 adj_p_vals, p_vals = estimate_p_values(r, n=n, lambda0=lam)
 
-np.save("../../data/par_cor.npy", r)
-np.save("../../data/adj_p_vals.npy", adj_p_vals)
-np.save("../../data/p_vals.npy", p_vals)
+np.save("../data/par_cor.npy", r)
+np.save("../data/adj_p_vals.npy", adj_p_vals)
+np.save("../data/p_vals.npy", p_vals)
